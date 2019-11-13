79e98f5bb9e405fe7706bacff02d7a56d4888d62
soaresCecilia <cecilia.oliveira.soares@gmail.com>
2019-11-13 16:14:47 +0000

    pasta

---
+++ src/com/jetbrains/Utilizador.java
@@ -0,0 +1,16 @@
+package com.jetbrains;
+
+public class Utilizador implements java.io.Serializable {
+    private int identificador;
+    private String nome;
+    private String email;
+    private String password;
+
+    public Utilizador(int id, String nome, String email, String pass) {
+        this.identificador = id;
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+    }
+
+}
